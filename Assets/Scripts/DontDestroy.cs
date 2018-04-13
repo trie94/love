24fdaba61c162b296952ffd4c667a2d48c309372
trie94 <trie60@gmail.com>
2018-04-13 02:21:08 -0400

    Add don't destroy script
    Add don't destroy script

--- Assets/Scripts/DontDestroy.cs
+++ Assets/Scripts/DontDestroy.cs
@@ -4,13 +4,8 @@ using UnityEngine;
 
 public class DontDestroy : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+	void Start ()
+    {
+        DontDestroyOnLoad(this);
 	}
 }
